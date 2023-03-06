ed8b0e6eb8a962d0b136fc8fb946c4f15e839c5c
Dmitrii <dimasik007@rambler.ru>
2023-03-06 23:02:45 +0300

    Добавил получение списка групп и добавил проверку списков

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,12 +2,18 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count_group() == 0:
         app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count_group() == 0:
         app.group.create(Group(header="test"))
     app.group.edit_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
