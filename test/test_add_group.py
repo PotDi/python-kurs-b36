9c62ab1b3f3d9c03ad1e6f174e04c534e16e8015
dmitriy.potapov <dmitriy.potapov@ispring.com>
2023-03-09 12:51:02 +0300

    Добавил метод получения списка групп из приложения

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
 from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="sdhfsdf", header="sdfsdf", footer="sdfdsdf"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) +1 == len(new_groups)
 
 
 def test_empty_add_group(app):
