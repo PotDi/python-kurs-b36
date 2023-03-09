ad77453dceab6674c601e3da55bcea3aa70c8e93
Dmitrii <dimasik007@rambler.ru>
2023-03-09 22:53:26 +0300

    Сортировка списка групп в тестах добавления и редактирования

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -7,9 +7,11 @@ def test_edit_group_name(app):
     group.id = old_groups[0].id
     if app.group.count_group() == 0:
         app.group.create(Group(name="test"))
-    app.group.edit_first_group(Group(name="New group"))
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 # def test_edit_group_header(app):
