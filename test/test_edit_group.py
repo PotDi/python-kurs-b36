84d69e9631025d6fd6dea42e7c980d65aeacb6d9
Dmitrii <dimasik007@rambler.ru>
2023-03-13 21:48:18 +0300

    Добавил рандом выбора групп при редактировании и удалении групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,18 @@
 from model.group import Group
+from random import randrange
 
 
 def test_edit_group_name(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="New group")
-    group.id = old_groups[0].id
     if app.group.count_group() == 0:
         app.group.create(Group(name="test"))
-    app.group.edit_first_group(group)
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    group = Group(name="New group")
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(index, group)
     assert len(old_groups) == app.group.count_group()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
