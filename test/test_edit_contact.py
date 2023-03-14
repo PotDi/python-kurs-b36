601b93d2ecea84b05a4c01d47b300332d1b4ca30
dmitriy.potapov <dmitriy.potapov@ispring.com>
2023-03-14 08:44:10 +0300

    Добавил рандом для выбора контакта при редактировании\удалении контактов

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,14 +1,16 @@
 from model.contact import Contact
+from random import randrange
 
 
 def test_edit_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="HELLO DIMA", lastname="test")
-    contact.id = old_contacts[0].id
     if app.contact.count_contact() == 0:
         app.contact.create_new_contact(Contact(firstname="THIS IS TEST"))
-    app.contact.edit_first_contact(contact)
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact = Contact(firstname="HELLO DIMA", lastname="test")
+    contact.id = old_contacts[index].id
+    app.contact.edit_contact_by_index(index, contact)
     assert len(old_contacts) == app.contact.count_contact()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
