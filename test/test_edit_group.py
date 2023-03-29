08fec5d8ce7076177d29e39bc6a07f821556a87c
dmitriy.potapov <dmitriy.potapov@ispring.com>
2023-03-29 09:04:49 +0300

    Поправил тест для удаления групп с данными из базы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,18 +4,17 @@ from model.group import Group
 from random import randrange
 
 
-def test_edit_group_name(app, db, json_groups):
-    new_group = json_groups
+def test_edit_group_name(app, db):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
     group = random.choice(old_groups)
-    group = Group(name="New group")
-    group.id = old_groups[index].id
-    app.group.edit_group_by_index(index, group)
+    group_edit_name = Group(name="New group")
+    group_edit_name.id = group.id
+    app.group.edit_group_by_id(group.id, group_edit_name)
     new_groups = db.get_group_list()
-    assert len(old_groups) == len(new_groups)
-    old_groups[index] = group
+    old_groups.remove(group)
+    old_groups.append(group_edit_name)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
