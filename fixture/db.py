270b3992dca26aac0a349154fc7cd15ac6415933
Dmitrii <dimasik007@rambler.ru>
2023-03-29 22:26:05 +0300

    Добавил проверку информации о контактах на главной странице

--- fixture/db.py
+++ fixture/db.py
@@ -38,7 +38,8 @@ class DbFixture:
                            "mobile, work, fax, email, email2, email3, "
                            "homepage, bday, byear, aday, amonth, ayear, "
                            "address2, phone2, notes from "
-                           "addressbook")
+                           "addressbook where deprecated='0000-00-00 "
+                           "00:00:00'")
             for row in cursor:
                 (id, firstname, middlename, lastname, nickname, company,
                  title, address, homephone, mobilephone, workphone, fax,
