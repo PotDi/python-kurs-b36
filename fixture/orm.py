edae21fc23d191d67c614eaf2c29f3748be683b3
dmitriy.potapov <dmitriy.potapov@ispring.com>
2023-04-05 09:22:01 +0300

    Доработал тест по проверки добавления контакта в группу

--- fixture/orm.py
+++ fixture/orm.py
@@ -24,8 +24,8 @@ class ORMFixture:
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
         groups = Set(lambda: ORMFixture.ORMGroup,
-                       table="address_in_groups", column="group_id",
-                       reverse="contacts", lazy=True)
+                     table="address_in_groups", column="group_id",
+                     reverse="contacts", lazy=True)
 
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user,
@@ -38,23 +38,25 @@ class ORMFixture:
             return Group(id=str(group.id), name=group.name,
                          header=group.header,
                          footer=group.footer)
+
         return list(map(convert, groups))
 
     def convert_contacts_to_model(self, contacts):
         def convert(contact):
             return Contact(id=str(contact.id), firstname=contact.firstname,
-                         lastname=contact.lastname)
+                           lastname=contact.lastname)
+
         return list(map(convert, contacts))
 
     @db_session
     def get_group_list(self):
         return self.convert_groups_to_model(select(g for g in
-                                                ORMFixture.ORMGroup))
+                                                   ORMFixture.ORMGroup))
 
     @db_session
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in
-                                                ORMFixture.ORMContact if
+                                                     ORMFixture.ORMContact if
                                                      c.deprecated is None))
 
     @db_session
@@ -66,3 +68,11 @@ class ORMFixture:
                                                      c.deprecated is None
                                                      and orm_group not in
                                                      c.groups))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if
+                                g.id == group.id))[0]
+        return self.convert_contacts_to_model(
+            select(c for c in ORMFixture.ORMContact if
+                   c.deprecated is None and orm_group in c.groups))
--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -14,4 +14,29 @@ def test_add_contact_in_group(app, db):
     contacts = db.get_contacts_without_group(group)
     contact = random.choice(contacts)
     app.contact.add_group_in_contact(contact.id, group.id)
+    assert clear_contact_db(contact) in db.get_contacts_in_group(group)
 
+
+def clear_db(s):
+    return "" if s is None else s
+
+
+def clear_contact_db(contact):
+    _ = clear_db
+    return Contact(firstname=_(contact.firstname),
+                   middlename=contact.middlename,
+                   lastname=_(contact.lastname), nickname=contact.nickname,
+                   title=contact.title, company=contact.company,
+                   address=_(contact.address), mobilephone=_(
+            contact.mobilephone),
+                   homephone=_(contact.homephone), workphone=_(
+            contact.workphone),
+                   fax=_(contact.fax), email=_(contact.email),
+                   secondaryemail=_(contact.secondaryemail),
+                   homepage=contact.homepage, bday=contact.bday,
+                   bmonth=contact.bmonth,
+                   byear=contact.byear, aday=contact.aday,
+                   amonth=contact.amonth, ayear=contact.ayear,
+                   secondaryaddress=contact.secondaryaddress, secondaryphone=_(
+            contact.secondaryphone),
+                   notes=contact.notes, id=contact.id)
