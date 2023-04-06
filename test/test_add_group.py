a646d14705587ccea1f078fb864f3929855f6a80
dmitriy.potapov <dmitriy.potapov@ispring.com>
2023-04-06 09:58:45 +0300

    Добавил чистку пробелов в списке групп базы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,5 +11,16 @@ def test_add_group(app, db, json_groups, check_ui):
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(
+        assert sorted(map(clear_group, new_groups), key=Group.id_or_max) == \
+               sorted(
             app.group.get_group_list(), key=Group.id_or_max)
+
+
+def clear_group(group):
+    _ = clear_space
+    return Group(id=group.id, name=_(group.name), header=_(group.header),
+                 footer=_(group.footer))
+
+
+def clear_space(s):
+    return " ".join(s.split()) if s is not None else ""
